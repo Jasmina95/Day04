0e175629035a799544bbb0d15e271b84361f2def
Jasmina Zejnilovic <jasmina.zejnilovic995@gmail.com>
2021-03-11 17:10:41 +0100

    Declare String Variables

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+    //Only change code below this line 
+    var myFirstName = "Jasmina";
+    var myLastName = "Zejnilovic";
+    var myHomeTown = "Brcko";
+    //Only change code above this line
+    return{
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main()); 
+module.exports = main;
